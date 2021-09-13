9e77178bbde9032b1c3a0a9bb72828d8027c088a
ahon54 <55165000+ahon54@users.noreply.github.com>
2021-09-13 13:18:07 -0700

    first commit

---
+++ src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
+
+const SigninPage = () => {
+    return (
+        <div>
+            <ScrollToTop />
+            <h1>Sign in page</h1>
+        </div>
+    )
+}
+
+export default SigninPage
